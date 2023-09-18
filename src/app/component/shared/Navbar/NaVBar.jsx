9ac519645a29d5958b2298736f2cf83f9f4f6a0d
Mozzammel Hossen <53122797+abirpuc@users.noreply.github.com>
2023-09-18 12:05:51 +0600

    change code

--- src/app/component/shared/Navbar/NaVBar.jsx
+++ src/app/component/shared/Navbar/NaVBar.jsx
@@ -1,11 +1,13 @@
-""
 import { getData } from '@/app/api/getData';
 
 import React from 'react';
 import NavContainer from './NavContainer';
 
-const NaVBar = async () => {
-    const navBar = await getData()
+const NaVBar = () => {
+    let navBar;
+    const callData = async() =>{
+        navBar = await getData()
+    }
     const menuList = navBar.menulists;
     const subMenu = navBar.menumaps
     return (
