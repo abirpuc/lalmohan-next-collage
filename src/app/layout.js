4b407dadb3f2f2c25a498e37916615eb78c4f067
Mozzammel Hossen <53122797+abirpuc@users.noreply.github.com>
2023-09-18 11:39:30 +0600

    change code

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { getData } from "./api/getData";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
-import bg from '../../public/background-img.png'
+// import bg from '../../public/background-img.png'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,12 +12,10 @@ export const metadata = {
 }
 
 export default async function RootLayout({ children }) {
-  const data = await getData()
-  const backgroundData = JSON.parse(data.web_presets)
-  const backgroundImage = backgroundData.background_img;
   return (
     <html lang="en">
-      <body className={inter.className} style={{backgroundImage:`url(${bg.src})`}}>
+      {/* style={{backgroundImage:`url(${bg.src})`}} */}
+      <body className={inter.className} >
         <div></div>
         <main>{children}</main>
       </body>
